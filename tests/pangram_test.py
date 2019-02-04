5d5bd685c7a06e699358261d98a553bc92cf54b4
Robert Boggs <r.boggs@guideline.com>
2019-02-03 20:49:15 -0800

    Reduce test code with helper

--- tests/pangram_test.py
+++ tests/pangram_test.py
@@ -3,24 +3,21 @@ from solutions import pangram
 
 class PanagramTest(unittest.TestCase):
   def test_invalid_length_text(self):
-    self.assertEqual(pangram.is_pangram_1("abc"), False)
-    self.assertEqual(pangram.is_pangram_2("abc"), False)
-    self.assertEqual(pangram.is_pangram_3("abc"), False)
+    self._evaluate_each_method("abc", False)
 
   def test_invalid_missing_chars_text(self):
-    self.assertEqual(pangram.is_pangram_1("abcdefghijklmnopqrstuvwxyyyy"), False)
-    self.assertEqual(pangram.is_pangram_2("abcdefghijklmnopqrstuvwxyyyy"), False)
-    self.assertEqual(pangram.is_pangram_3("abcdefghijklmnopqrstuvwxyyyy"), False)
+    self._evaluate_each_method("abcdefghijklmnopqrstuvwxyyyy", False)
 
   def test_valid_text(self):
-    self.assertEqual(pangram.is_pangram_1("abcdefghijklmnopqrstuvwxzyaaaaazzzzeee"), True)
-    self.assertEqual(pangram.is_pangram_2("abcdefghijklmnopqrstuvwxzyaaaaazzzzeee"), True)
-    self.assertEqual(pangram.is_pangram_3("abcdefghijklmnopqrstuvwxzyaaaaazzzzeee"), True)
+    self._evaluate_each_method("abcdefghijklmnopqrstuvwxzyaaaaazzzzeee", True)
 
   def test_valid_with_spaces_text(self):
-    self.assertEqual(pangram.is_pangram_1("the quick brown fox jumps over the lazy dog"), True)
-    self.assertEqual(pangram.is_pangram_2("the quick brown fox jumps over the lazy dog"), True)
-    self.assertEqual(pangram.is_pangram_3("the quick brown fox jumps over the lazy dog"), True)
+    self._evaluate_each_method("the quick brown fox jumps over the lazy dog", True)
+
+  def _evaluate_each_method(self, text, expected_value):
+    for index in range(3):
+      method_name = f'is_pangram_{index + 1}'
+      self.assertEqual(getattr(pangram, method_name)(text), expected_value)
 
 if __name__ == '__main__':
   unittest.main()
