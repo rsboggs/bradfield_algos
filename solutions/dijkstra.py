d225e57849fba84c8f96f7536ec64b44aeb15f78
Robert Boggs <r.boggs@guideline.com>
2019-02-18 20:38:07 -0800

    More edge cases

--- solutions/dijkstra.py
+++ solutions/dijkstra.py
@@ -1,4 +1,4 @@
-from queue import PriorityQueue
+import heapq
 from collections import defaultdict
 
 def network_delay_time(times, N, K):
@@ -15,20 +15,30 @@ def network_delay_time(times, N, K):
     distances[value] = float("inf")
   distances[K] = 0
 
-  priority_queue = PriorityQueue()
+  priority_queue = []
+  entries = {}
   for node_source, neighbors in graph.items():
-    bucket = (node_source, neighbors)
-    priority_queue.put(bucket, distances[node_source])
+    bucket = [distances[node_source], node_source]
+    heapq.heappush(priority_queue, bucket)
+    entries[node_source] = bucket
 
-  while not priority_queue.empty():
-    node_source, neighbors = priority_queue.get()
+  while len(priority_queue) > 0:
+    _queue_distance, node_source = heapq.heappop(priority_queue)
+    neighbors = graph[node_source]
 
     for neighbor_value, distance in neighbors.items():
-      if node_source == K:
+      if node_source == K and distances[neighbor_value] > distance:
         distances[neighbor_value] = distance
+        if neighbor_value in entries:
+          entries[neighbor_value][0] = distance
       elif distances[node_source] != float("inf") and distances[neighbor_value] > distances[node_source] + distance:
         distances[neighbor_value] = distances[node_source] + distance
+        if neighbor_value in entries:
+          entries[neighbor_value][0] = distances[node_source] + distance
 
-  maximum = max(distances.values())
+  values = distances.values()
+  if len(values) != N:
+    return -1
+  maximum = max(values)
   return maximum if maximum < float("inf") else -1
 
