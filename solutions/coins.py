4e168f1ae64af8e2182daaa18851a929f11ae244
Robert Boggs <r.boggs@guideline.com>
2019-02-24 18:06:07 -0800

    Coin change

--- solutions/coins.py
+++ solutions/coins.py
@@ -10,9 +10,32 @@ def minimum_count(coins, amount, count = 0):
     if amount - coin >= 0:
       next.append(minimum_count(coins, amount - coin, count + 1))
 
+  if not next:
+    return -1
   minimum = min(next)
   maximum = max(next)
   if minimum == -1 and maximum == -1:
     return -1
   else:
     return minimum
+
+# Memoize Recursion
+def minimum_count2(coins, amount, saved = {}):
+  if amount < 0:
+    return -1
+  elif amount == 0:
+    return 0
+
+  memoize_key = (tuple(coins), amount)
+  if memoize_key in saved:
+    return saved[memoize_key]
+
+  count = None
+  for coin in sorted(coins, reverse=True):
+    if amount - coin >= 0:
+      added_count = minimum_count2(coins, amount - coin, saved)
+      if added_count != -1 and (count == None or added_count < count):
+        count = added_count + 1
+
+  saved[memoize_key] = -1 if count == None else count
+  return saved[memoize_key]
--- tests/coins_test.py
+++ tests/coins_test.py
@@ -2,9 +2,37 @@ import unittest
 from solutions import coins
 
 class CoinsTest(unittest.TestCase):
-  def test_can_solve_simple_case(self):
+  def test_can_solve_one_coin(self):
     input_coins = [1, 2, 5]
-    self.assertEqual(coins.minimum_count(input_coins, 11), 3)
+    self.assertEqual(coins.minimum_count2(input_coins, 2), 1)
+
+  def test_can_solve_two_coin(self):
+    input_coins = [1, 2, 5]
+    self.assertEqual(coins.minimum_count2(input_coins, 3), 2)
+
+  def test_can_solve_case(self):
+    input_coins = [1, 2, 5]
+    self.assertEqual(coins.minimum_count2(input_coins, 7), 2)
+
+  def test_can_solve_case2(self):
+    input_coins = [1, 2, 5]
+    self.assertEqual(coins.minimum_count2(input_coins, 11), 3)
+
+  def test_can_solve_case3(self):
+    input_coins = [2,5,10,1]
+    self.assertEqual(coins.minimum_count2(input_coins, 27), 4)
+
+  def test_can_solve_case4(self):
+    input_coins = [186,419,83,408]
+    self.assertEqual(coins.minimum_count2(input_coins, 6249), 20)
+
+  def test_will_return_negative_one_if_not_possible(self):
+    input_coins = [2, 5]
+    self.assertEqual(coins.minimum_count2(input_coins, 3), -1)
+
+  def test_will_return_negative_one_if_not_possible2(self):
+    input_coins = [2]
+    self.assertEqual(coins.minimum_count2(input_coins, 3), -1)
 
 if __name__ == "__main__":
   unittest.main()
